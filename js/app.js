2d863b9842fba0cba7c8724e7b73e45b5798fe0a
Jon Welker <JWelker110@gmail.com>
2015-10-29 16:46:04 -0500

    Add initial speed to bugs

--- js/app.js
+++ js/app.js
@@ -1,8 +1,13 @@
+var xArray = [0, 100, 200, 300, 400];
+var yArray = [-25, 50, 135, 220, 305, 390];
+
 // Enemies our player must avoid
 var Enemy = function() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-
+    this.x = -100;
+    this.y = yArray[(Math.ceil(Math.random() * 10) % 3) + 1];
+    this.speed = 100;
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
@@ -14,6 +19,8 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x = ((this.x + 100 + (this.speed * dt)) % 605) - 100;
+
 };
 
 // Draw the enemy on the screen, required method for game
@@ -24,11 +31,34 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+var Player = function(){
+    this.x = 200;
+    this.y = -25;
+
+    this.sprite = 'images/char-boy.png';
+};
+
+Player.prototype.update = function(){
+
+}
+
+Player.prototype.render = function() {
+
+}
+
+Player.prototype.handleInput = function(direction){
+    if (direction == 'left') {
+        this.x = 0;
+    }
+};
 
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
+var allEnemies = [new Enemy()];
+var player = new Player();
+
 
 
 
